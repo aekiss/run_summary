6d04d679c126b2e7914ef5bb77563d1919992548
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2018-10-22 15:24:52 +1100

    parse_pbs_log now working

--- run_summary.py
+++ run_summary.py
@@ -11,6 +11,7 @@ Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.txt
 # import copy
 # import time
 # import datetime
+import re
 
 def parse_pbs_log(fname):
     """
@@ -36,51 +37,68 @@ def parse_pbs_log(fname):
 
     """
     def null(l):
-        return l[0]
+        return l[1]
 
     def getrun(l):
         return int(l[4].rstrip('"'))
 
     def getjob(l):
-        return int(l[0].split('.')[0])
+        return int(l[1].split('.')[0])
 
     def getint(l):
-        return int(l[0])
+        return int(l[1])
 
     def getfloat(l):
-        return float(l[0])
+        return float(l[1])
 
     def getsec(l):  # convert hh:mm:ss to sec
-        return sum(x * int(t) for x, t in zip([3600, 60, 1], l[0].split(':')))
+        return sum(x * int(t) for x, t in zip([3600, 60, 1], l[1].split(':')))
 
     def getdatetime(l):
         return l[0]+'T'+l[1].rstrip(':')
 
-    search_items = {  # keys are strings to search for; items are functions to apply to list of strings following key
+    def getbytes(l):  # assumes PBS log info uses binary prefixes - TODO: check
+        s = l[1]
+        n = float(re.match('((\.|\d)+)', s).groups()[0])  # numerical part
+        units = {'KB': 2**10,
+                 'MB': 2**20,
+                 'GB': 2**30,
+                 'TB': 2**40}
+        return int(round(n*units[s[-2:]]))
+
+    search_items = {  # keys are strings to search for; items are functions to apply to whitespace-delimited list of strings following key
         'git commit': getrun,
         'Resource Usage on': getdatetime,
-        'Job Id:': getjob,
+        'Job Id': getjob,
         'Project': null,
-        'Exit Status:': getint,
-        'Service Units:': getfloat,
-        'NCPUs Requested:': getint,
-        'NCPUs Used:': getint,
-        'CPU Time Used:': getsec,
-        'Memory Requested:': null,  # TODO: convert Gb, Tb etc to bytes?
-        'Memory Used:': null,  # TODO: convert Gb, Tb etc to bytes?
-        'Walltime requested:': getsec,
-        'Walltime Used:': getsec,
-        'JobFS requested:': null,  # TODO: convert Gb, Tb etc to bytes?
-        'JobFS used:': null}  # TODO: convert Gb, Tb etc to bytes?
+        'Exit Status': getint,
+        'Service Units': getfloat,
+        'NCPUs Requested': getint,
+        'NCPUs Used': getint,
+        'CPU Time Used': getsec,
+        'Memory Requested': getbytes,
+        'Memory Used': getbytes,
+        'Walltime requested': getsec,
+        'Walltime Used': getsec,
+        'JobFS requested': getbytes,
+        'JobFS used': getbytes}
     parsed_items = search_items.fromkeys(search_items, None)
 
     with open(fname, 'r') as infile:
         for line in infile:
+            #  NB: subsequent matches will replace earlier ones
             for key, op in search_items.items():
                 try:
                     parsed_items[key] = op(line.split(key)[1].split())
                 except:
                     continue
+
+    # change to more self-explanatory keys
+    rename_keys = {'git commit': 'Run number',
+                   'Resource Usage on': 'Run completion date'}
+    for oldkey, newkey in rename_keys.items():
+        parsed_items[newkey] = parsed_items.pop(oldkey)
+
     return parsed_items
 
 parse_pbs_log('test/01deg_jra55_iaf.o1399186')
